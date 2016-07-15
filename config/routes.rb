3df03408b82ca1c9a1443c1e38f3e777885bbf9f
cristianbarbaro <cristiandanielbarbaro@gmail.com>
2016-07-15 20:41:54 -0300

    Agregado un controlador para Home.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :marks
-  resources :categories
-  resources :articles
-  resources :providers
-  resources :stocks
-  resources :sales
-  resources :historic
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'home#index'
+
+    resources :marks
+    resources :categories
+    resources :articles
+    resources :providers
+    resources :stocks
+    resources :sales
+    resources :historics
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
