316787e86623382dd73c60b5f6297b8a7e4bb924
cristianbarbaro <cristiandanielbarbaro@gmail.com>
2016-07-16 10:52:12 -0300

    Agregaga lógicas y vistas para ABM proveedores.

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,22 +1,55 @@
 class ProvidersController < ApplicationController
-  def index
-  end
+    before_action :set_provider, only: [:show, :edit, :update, :destroy]
+    helper ProvidersHelper
 
-  def show
-  end
+    def index
+        @providers = Provider.all
+    end
 
-  def new
-  end
+    def show
+    end
 
-  def edit
-  end
+    def new
+        @provider = Provider.new
+    end
 
-  def create
-  end
+    def edit
+    end
 
-  def update
-  end
+    def create
+        @provider = Provider.new(provider_params)
+        if @provider.save
+            flash[:success] = 'El proveedor se ha creado correctamente.'
+            redirect_to @provider
+        else
+            render :edit
+        end
+    end
 
-  def destroy
-  end
+    def update
+        if @provider.update(provider_params)
+            flash[:success] = 'El proveedor se ha actualizado correctamente.'
+            redirect_to @provider
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        if @provider.destroy
+            flash[:success] = "El proveedor se ha eliminado correctamente."
+        else
+            flash[:alert] = "No se puede eliminar el proveedor porque tiene artículos o categorías asociados."
+        end
+        redirect_to providers_url
+    end
+
+    private
+        def set_provider
+            @provider = Provider.find(params[:id])
+        end
+
+        def provider_params
+            params.require(:provider).permit(:name, :category_id)
+        end
 end
--- app/helpers/providers_helper.rb
+++ app/helpers/providers_helper.rb
@@ -1,2 +1,5 @@
 module ProvidersHelper
+    def get_category_name(category_id)
+        Category.find(category_id).name
+    end
 end
