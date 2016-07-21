e2989144b0ba2d8cd92211b9e617b08486390a4d
cristianbarbaro <cristiandanielbarbaro@gmail.com>
2016-07-21 20:06:57 -0300

    Validaciones actualizadas para stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,4 +3,6 @@ class Stock < ApplicationRecord
     validates_presence_of :article
 
     validates :current_amount, :minimum_amount, presence: true
+    validates :current_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validates :minimum_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
