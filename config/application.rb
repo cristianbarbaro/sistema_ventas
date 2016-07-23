5b3fb1c46b8348c3d36256924fa66691e758e8db
cristianbarbaro <cristiandanielbarbaro@gmail.com>
2016-07-23 16:31:08 -0300

    Configuracion de fixtures para tests.

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Src
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.web_console.whitelisted_ips = '10.0.0.1'
+    # config.web_console.whitelisted_ips = '10.0.0.1'
     config.i18n.default_locale = :es
   end
 end
