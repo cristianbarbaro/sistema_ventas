550c5a662669daf2ce0d508221a9a0d7e3dd86ec
cristianbarbaro <cristiandanielbarbaro@gmail.com>
2016-08-31 17:29:39 -0300

    Usamos ransack para las búsquedas de las ventas (intervalos de tiempo)

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -5,35 +5,14 @@ class SalesController < ApplicationController
     end
 
     def index
-        ok = true
-        # Si tenemos una fecha, validamos también los campos de las horas.
-        if not params[:day].nil?
-            day = params[:day]
-            if not params[:initial_hour].empty? and not params[:final_hour].empty?
-                if params[:initial_hour] < params[:final_hour]
-                    initial = params[:initial_hour]
-                    final = params[:final_hour]
-                else
-                    flash[:alert] = "Intervalo de horas inválido."
-                    ok = false
-                end
-            else
-                flash[:alert] = "Debe ingresar ambos horarios."
-                ok = false
-            end
-        # En el caso de que sea nil (se accede a la página sin parámetros), muestro los valores del día actual:
+        if params[:q].nil?
+            @q = Sale.ransack(date_created_at_eq: Date.today)
+            @sales = @q.result
         else
-            day = Date.today
-            initial = "00:00"
-            final = "23:59"
+            @q = Sale.ransack(params[:q])
+            @sales = @q.result
         end
-        if ok
-            @sales = Sale.where("date(created_at) = ? and time(created_at) >= ? and time(created_at) <= ?", day, initial, final)
-            @total_interval = @sales.sum(:total_price)
-        end
-        # @q = Sale.ransack(params[:q])
-        # @sales = @q.result.paginate(:page => params[:page])
-
+        @total_interval = @sales.sum(:total_price)
     end
 
     def show
