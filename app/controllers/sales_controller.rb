c6fb8feb9025e3b97562aea41bd64aa39152603c
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2016-12-03 17:36:37 -0300

    Se arregla problemas con listado de ventas

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -7,14 +7,24 @@ class SalesController < ApplicationController
     end
 
     def index
+        # Cuando se accede a esta página, están vacíos los params, entonces muestro lo del día de la fecha.
         if params[:q].nil?
-            @q = Sale.ransack(date_created_at_eq: Date.today)
+            @q = Sale.ransack(date_created_at_eq: Date.today.beginning_of_day)
             @sales = @q.result
-        else
-            @q = Sale.ransack(params[:q])
+        # El caso que más importa es cuando comienza a buscar. Necesito saber la fecha de inicio, siendo por defecto, el actual.
+        elsif params[:q][:created_at_gt].empty?
+            parameters = params[:q]
+            parameters[:created_at_gt] = Date.today.beginning_of_day
+            @q = Sale.ransack(parameters)
             @sales = @q.result.uniq
             check_category_field params[:q][:sale_lines_article_category_id_eq]
             check_article_field params[:q][:sale_lines_article_id_eq]
+        # Cuando hay fecha de inicio, puedo tomar todos los parámetros sin problemas.
+        else
+          @q = Sale.ransack(params[:q])
+          @sales = @q.result.uniq
+          check_category_field params[:q][:sale_lines_article_category_id_eq]
+          check_article_field params[:q][:sale_lines_article_id_eq]
         end
         @total_interval = @sales.sum(:total_price)
     end
