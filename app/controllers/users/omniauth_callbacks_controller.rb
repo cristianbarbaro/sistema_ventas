28ae11039c1f30aed7058f24d8066cea49a30209
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2016-12-13 13:54:40 -0300

    Flash messages to login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,8 +5,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      flash[:success] = 'El usuario ha iniciado sesión correctamente.'
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
+      flash[:alert] = 'El email ya existe en la base de datos para otro usuario.'
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
@@ -16,8 +18,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
+      flash[:success] = 'El usuario ha iniciado sesión correctamente.'
       set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
     else
+      flash[:alert] = 'El email ya existe en la base de datos para otro usuario.'
       session["devise.google_oauth2_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
