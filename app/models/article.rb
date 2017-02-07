87c5d505523a45cc4ab6a9e57aed13b76d2a56e0
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2017-02-07 14:57:00 -0300

    Funcion en modelo article no usada eliminada

--- app/models/article.rb
+++ app/models/article.rb
@@ -20,11 +20,6 @@ class Article < ApplicationRecord
     validates :code, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
     validates_uniqueness_of :code
 
-    def self.search(search)
-        self.where(
-            "code = ? or name LIKE ? or description LIKE ?", search, "%#{search}%", "%#{search}%")
-    end
-
     def create_historic(new_cost_price)
         self.historics.create({cost_price: new_cost_price})
     end
