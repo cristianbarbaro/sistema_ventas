688574b03f3c56e89afa9660d1460f01553c3fc7
Cristian Daniel Barbaro <cbarbaro@cert.unlp.edu.ar>
2018-04-24 13:58:12 -0300

    correcciones en archivos docker

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Installing Ruby and rbenv.
-sudo apt-get install -y autoconf bison build-essential lib{ssl,yaml,sqlite3}-dev libreadline6{,-dev} zlib1g{,-dev}
-sudo apt-get install nodejs
+sudo apt-get update && apt-get install -y autoconf bison build-essential lib{ssl,yaml,sqlite3,mysqlclient}-dev libreadline6{,-dev} zlib1g{,-dev} git mysql-server-core-5.5
+sudo apt-get install -y nodejs
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 source ~/.bashrc
+export PATH="$HOME/.rbenv/bin:$PATH"
 rbenv install 2.3.0
 rbenv global 2.3.0
