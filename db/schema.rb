f41fd4851563ad88b013c30ba2bb236a6e9e8763
Cristian Daniel Barbaro <cbarbaro@cert.unlp.edu.ar>
2018-06-19 14:29:02 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_01_153223) do
+ActiveRecord::Schema.define(version: 2018_06_12_202230) do
 
   create_table "article_providers", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "article_id"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 2018_06_01_153223) do
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["mark_id"], name: "index_articles_on_mark_id"
+  end
+
+  create_table "branches", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
